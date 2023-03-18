79eec87d7cdda2e25ccc3a50184833becae5d49a
LOVKUSH <lovkushyadav9888@gmail.com>
2023-03-18 20:13:10 +0530

    First basic counter app using useState

--- counter-app-1/src/App.js
+++ counter-app-1/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  //let count = 0;
+
+  //Its the array Destructuring method
+  //count == variable we want to use is state = 0; & setCount is function name
+
+  const [count, setCount] = useState(0);
+  //console.log(useState(0));
+
+  function handleAdd (){
+    /*count += 1;
+    console.log(count);*/
+    setCount(count+1);
+
+  }
+
+  function handleSub (){
+    setCount(count-1);
+  }
+
+  function handleReset(){
+    setCount(0);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="main">
+    <h1>Counter App</h1>
+      <p>{count}</p>
+      <button onClick={handleAdd} className="add">ADD</button>
+      <button onClick={handleSub} className="sub">SUB</button>
+      <button onClick={handleReset} className="Reset">RESET</button>
     </div>
+      
+    </>
   );
 }
 
---
+++ counter-app-1/src/Components/first.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const first = () => {
+  return (
+    <div>
+      <h1>my name is Lovkush</h1>
+      
+    </div>
+  )
+}
+
+export default first;
