b00c95dbb27a9199310fe5c85c3e206ec10cd700
LOVKUSH <lovkushyadav9888@gmail.com>
2023-03-19 13:29:02 +0530

    Deleting the handle-React filters

--- project-3/src/App.js
+++ project-3/src/App.js
@@ -30,3 +30,31 @@ function App() {
 }
 
 export default App;
+
+
+//Read about filters
+/*import React, { useState } from 'react';
+
+const App = () => {
+  const [items, setItems] = useState([
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' },
+  ]);
+
+  const filteredItems = items.filter(item => item.name.includes('2'));
+
+  return (
+    <div>
+      <h1>Filtered Items</h1>
+      <ul>
+        {filteredItems.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
+*/
