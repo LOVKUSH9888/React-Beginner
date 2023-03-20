2f89baeb1ae2c8c28f86a48ca75c0ddb5c0271dd
LOVKUSH <lovkushyadav9888@gmail.com>
2023-03-20 21:37:49 +0530

    react-Forms

---
+++ Forms.jsx
@@ -0,0 +1,14 @@
+function MyForm(){
+    return (
+        <form>
+            <label>Name
+            <input type="text" />
+            </label>
+        </form>
+    )
+}
+
+//Events in forms : -
+//1. onChange event
+//2. onClick event
+//3. onSubmit event
