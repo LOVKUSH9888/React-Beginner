550a454d8c4484424c029f62ec0e9a46ae474920
LOVKUSH <lovkushyadav9888@gmail.com>
2023-03-21 00:48:10 +0530

    Project-4=React Forms

---
+++ project-4-forms/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+import RegForm from './Components/RegForm';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <RegForm/>
+    </>
+  );
+}
+
+export default App;
---
+++ project-4-forms/src/Components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h3>Registration</h3>
+    </div>
+  )
+}
+
+export default Header;
