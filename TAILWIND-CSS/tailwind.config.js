7832c1ee670022eaaacad2c3a802d0458ba9dd80
LOVKUSH <lovkushyadav9888@gmail.com>
2023-03-25 20:51:10 +0530

    Completing the Tailwind CSS

---
+++ TAILWIND-CSS/tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        screens: {
+          sm: '480px',
+          md: '768px',
+          lg: '976px',
+          xl: '1440px',
+          color: {
+            logo: #f1f1f
+          }
+        }
+    }
+}
