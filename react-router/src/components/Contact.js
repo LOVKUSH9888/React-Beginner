160ffe1c7b0c1c641444aa630ff0cb0309b331d8
LOVKUSH <lovkushyadav9888@gmail.com>
2023-03-29 21:37:56 +0530

    Route Parameters

--- react-router/src/components/Contact.js
+++ react-router/src/components/Contact.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Contact = () => {
+
+  handleSubmit = () => {
+    console.log("-----");
+    //navigate("/Home");
+    return navigate("/Home");
+  }
+
   return (
     <div>
       <h1>Contact - me</h1>
+      <button onClick={handleSubmit}>Back to Home</button>
     </div>
   )
 }
 
 export default Contact
+
+
