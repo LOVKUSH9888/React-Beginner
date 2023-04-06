b338de567846c2a1540bae8af3d0f22d2aa350f0
LOVKUSH <lovkushyadav9888@gmail.com>
2023-04-06 23:05:47 +0530

    adding Webpack

--- FIREBASE/basics.jsx
+++ FIREBASE/basics.jsx
@@ -2,3 +2,7 @@
 ///SupaBase, Apprite - Similar to all
 
 // #Day 2 - Firebase
+///dist = Distributable files folder
+npm init -y
+npm i webpack {/*Version  webpack = It bundles all the scattered Typescript's modules/files and bundle them into a single index. js Javascript file.*/ }
+
