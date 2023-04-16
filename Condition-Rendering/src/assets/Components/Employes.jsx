d3e29552c94ff55bab60d7d165e9b450689a3440
LOVKUSH <lovkushyadav9888@gmail.com>
2023-04-16 21:34:44 +0530

    map data

---
+++ Condition-Rendering/src/assets/Components/Employes.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Employes = () => {
+  return (
+    let employees = [
+        {
+          id: 001,
+          name: "Tommy",
+          age: 23,
+          city: "New York",
+        },
+        {
+          id: 002,
+          name: "Mike",
+          age: 27,
+          city: "Detroit",
+        },
+        {
+          id: 003,
+          name: "Lisa",
+          age: 25,
+          city: "Chicago",
+        },
+        {
+          id: 004,
+          name: "Kane",
+          age: 31,
+          city: "New York",
+        },
+        {
+          id: 005,
+          name: "Mark",
+          age: 37,
+          city: "Chicago",
+        },
+      ];
+  )
+}
+
+export default Employes;
