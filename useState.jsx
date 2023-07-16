1226c7fb571264974332b77c9cd8f8ffe03fd0ca
LOVKUSH <lovkushyadav9888@gmail.com>
2023-07-16 21:49:07 +0530

    ::::::revising the axis fetch

--- useState.jsx
+++ useState.jsx
@@ -28,4 +28,5 @@ return (
 //Given interview- idesign tech = GFailed = axios fetch data and map that.
 //Learning ES6 concepts
 //Learning ES6 concepts-Aysnchronous+promises + asynsc await
+// Revising - Axios and map function used on - 15-July-2023
 
