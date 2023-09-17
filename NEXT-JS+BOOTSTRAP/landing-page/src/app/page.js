a48a17a154abec626490b10d2ba23d910660d172
LOVKUSH <lovkushyadav9888@gmail.com>
2023-09-17 17:40:28 +0530

    Adding Bootstrap in nextJS

---
+++ NEXT-JS+BOOTSTRAP/landing-page/src/app/page.js
@@ -0,0 +1,15 @@
+'use client'
+
+import Footer from "./Components/Footer"
+import Navbar from "./Components/Navbar"
+
+const page = () => {
+  return (
+    <>
+      <Navbar/>
+      <Footer/>
+    </>
+  )
+}
+
+export default page
